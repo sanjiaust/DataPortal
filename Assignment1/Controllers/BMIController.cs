f0115c57e4e0627141b7e8559d1ce823c96e6731
Sanji <ridwanul.cse.20210104051@aust.edu>
2024-07-27 22:49:46 +0600

    Initial project commit

---
+++ Assignment1/Controllers/BMIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    public class BMIController : Controller
+    {
+        // GET: BMI
+        public ActionResult BMICalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CalculateBMI(BMIViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("BMICalculator", model);
+            }
+
+            // Calculate BMI
+            double heightInMeters = ((model.Feet * 12) + model.Inches) * 0.0254;
+            double bmi = model.Weight / (heightInMeters * heightInMeters);
+
+            ViewBag.BMI = bmi;
+
+            return View("BMICalculator", model);
+        }
+    }
+}
---
+++ Assignment1/Controllers/DetailsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class DetailsController : Controller
+    {
+        // GET: Details
+        public ActionResult Index()
+        {
+            if (Session["UserId"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitDetails(UserDetailsViewModel model)
+        {
+            if (Session["UserId"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            return View("DetailsSubmitted", model);
+        }
+    }
+}
